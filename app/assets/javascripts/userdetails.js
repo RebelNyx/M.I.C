761ac527d9250c1aa87ace2183e40bc7c529529a
isnikhiljain <jainniks108@gmail.com>
2017-07-17 02:40:59 +0530

    Form Views Modified

---
+++ app/assets/javascripts/userdetails.js
@@ -0,0 +1,6 @@
+/*var description = document.getElementById('userdetail_artist_type')
+
+description.addEventListener('keyup' , function () {
+	    description.style.height = "20px";
+    description.style.height = (description.scrollHeight)+"px";
+});/
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :userdetails , only: [:create , :new]
   
   devise_for :users
+
+  post 'upload' => 'userdetails#upload'
   
 
 

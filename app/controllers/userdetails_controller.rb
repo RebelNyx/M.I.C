81533fab54f001d519a2f4a85d97074004228861
isnikhiljain <jainniks108@gmail.com>
2017-07-18 02:08:04 +0530

    update

--- app/controllers/userdetails_controller.rb
+++ app/controllers/userdetails_controller.rb
@@ -65,6 +65,10 @@ class UserdetailsController < ApplicationController
     end
   end
 
+  def upload
+  end
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_userdetail

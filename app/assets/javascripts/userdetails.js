6d48f6e9ea1b79a32139c63cf519d3ddc1482e83
isnikhiljain <jainniks108@gmail.com>
2017-07-18 21:42:58 +0530

    Profile css

--- app/assets/javascripts/userdetails.js
+++ app/assets/javascripts/userdetails.js
@@ -1,6 +1,1 @@
-/*var description = document.getElementById('userdetail_artist_type')
 
-description.addEventListener('keyup' , function () {
-	    description.style.height = "20px";
-    description.style.height = (description.scrollHeight)+"px";
-});/
